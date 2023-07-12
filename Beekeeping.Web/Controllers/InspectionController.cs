465b8929b98f82737f3a0dd04848657c4f589441
SiyanaPetkova <s.petkova323@gmail.com>
2023-07-12 09:37:11 +0300

    Add some pictures tothe forms and cards

---
+++ Beekeeping.Web/Controllers/InspectionController.cs
@@ -0,0 +1,14 @@
+﻿namespace Beekeeping.Web.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize]
+    public class InspectionController : Controller
+    {
+        public IActionResult All()
+        {
+            return View();
+        }
+    }
+}
