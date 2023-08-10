02faf29264449095b14f4a712248bf943b994547
SiyanaPetkova <s.petkova323@gmail.com>
2023-08-10 16:43:38 +0300

    Add AboutUs view

--- Beekeeping.Web/Controllers/HomeController.cs
+++ Beekeeping.Web/Controllers/HomeController.cs
@@ -36,6 +36,11 @@
             return View();
         }
 
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
